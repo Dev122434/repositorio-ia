246d18a2d8e475d188bf568154b6466e3eac8b5f
Dev122434 <68534leo@universidaddeleon.edu.mx>
2025-11-03 07:57:18 -0600

    Ventana principal corregida

--- load/load_ventana_principal.py
+++ load/load_ventana_principal.py
@@ -1,15 +1,24 @@
 from PyQt5 import QtWidgets, uic
+from load.load_ventana_modelos_basicos import LoadVentanaModelosBasicos
 
 class LoadVentanaPrincipal(QtWidgets.QMainWindow):
     def __init__(self):
         super().__init__()
         # Carga la interfaz grafica
-        uic.loadUi('interfaces/ventana_principal.ui', self)
+        uic.loadUi("interfaces/ventana_principal.ui", self)
         # Maximizar la ventana
         self.showMaximized()
         # Hacer referencia al item del menu
-        self.actionSalir.triggered.connect(self.cerrarVentana)
+        self.actionBasicos.triggered.connect(self.abrirVentanaBasicos)
+        self.actionLangChain.triggered.connect(self.abrirLangchain)
         self.actionSalir.triggered.connect(self.cerrarVentana)
 
     def cerrarVentana(self):
         self.close()
+
+    def abrirVentanaBasicos(self):
+        self.basicos = LoadVentanaModelosBasicos()
+        self.basicos.exec_()
+
+    def abrirLangchain(self):
+        pass
